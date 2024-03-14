88259836d6057091d2ade1fff27c5137f3652998
robbitt07 <robertgoss07@live.com>
2024-03-14 16:39:17 -0500

    [yadet-1] Table Alias Bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = ["pyodbc"]
 
 setup(
     name="yadet",
-    version="1.3.0",
+    version="1.3.1",
     author="Robert Goss",
     author_email="robertgoss07@gmail.com",
     packages=[
